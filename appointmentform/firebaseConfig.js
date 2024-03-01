65269ad48d49aaa16aa55e2fedcc53826c3765ef
MXDRIGAL <145941680+MXDRIGAL@users.noreply.github.com>
2024-03-01 19:45:30 +0700

    Add files via upload

---
+++ appointmentform/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDFwRuToxOiUx6-bzHaTIInXJvobrZJ9Hc",
+    authDomain: "appointmentformdata.firebaseapp.com",
+    databaseURL: "https://appointmentformdata-default-rtdb.firebaseio.com",
+    projectId: "appointmentformdata",
+    storageBucket: "appointmentformdata.appspot.com",
+    messagingSenderId: "1078093822860",
+    appId: "1:1078093822860:web:4f83e7f1da50456893f4a9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
